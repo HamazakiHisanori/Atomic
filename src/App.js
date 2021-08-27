345fe5f0b4789f31a3ef8a6041a8245f044fded7
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-08-27 17:48:52 +0900

    Atom create button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+    </div>
+  );
 }
---
+++ src/components/atoms/buttons/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: white;
+  padding: 8px 24px;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.5;
+  }
+`;
