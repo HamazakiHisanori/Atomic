b97e26874123db0de4304caeafed8dbf6f261ff5
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-08-31 15:11:24 +0900

    Recoil how to use

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,10 +1,12 @@
-import { useContext } from "react";
+//import { useContext } from "react";
 import styled from "styled-components";
-import { UserContext } from "../../providers/UserProvider";
+// import { UserContext } from "../../providers/UserProvider";
 import { SecondaryButton } from "../atoms/buttons/SecondaryButton";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 import { HeaderOnly } from "../templates/HeaderOnly";
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/userState";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -21,7 +23,9 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { userInfo, setUserInfo } = useContext(UserContext);
+  // const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
   const onClickChange = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
 
   return (
